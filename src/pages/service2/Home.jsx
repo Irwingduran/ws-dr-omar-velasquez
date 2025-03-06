6e289ec93bcb16914f83d3f13a72c04b2050ba6e
Irwing <86335744+Irwingduran@users.noreply.github.com>
2025-03-05 22:32:23 -0600

    Update Home.jsx

--- src/pages/service2/Home.jsx
+++ src/pages/service2/Home.jsx
@@ -45,7 +45,7 @@ const ServiceDetailsAlternative = () => {
               <FaUserMd className="text-4xl text-white" />
             </div>
           </div>
-          <div className="relative pb-[177.78%] h-0 overflow-hidden rounded-lg shadow-lg">
+          <div className="relative pb-[177.78%] md:pb-[56.25%] h-0 overflow-hidden rounded-lg shadow-lg">
             <iframe
               src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F417176356992889%2F&show_text=false&width=267&t=0"
               className="absolute top-0 left-0 w-full h-full"
